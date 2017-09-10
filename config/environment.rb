4b1b5623cd40c3f97dab297826a24e757f69791f
Richard Burd <richardburdx@yahoo.com>
2017-09-10 21:09:08 +0000

    added executable permission and folders

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
-require 'nokogriri'
+# this video here has relevant info for establishing requirements:
+# https://www.youtube.com/watch?v=XBgZLm-sdl8
+require 'nokogiri'
 require 'open-uri'
-require 'pry' 
-require_all 'lib'
+require 'pry'
+# right now this require_all isn't working so I commented it out till I get to fixing it.
+# require_all 'lib'
