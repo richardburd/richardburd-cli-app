ec65dad324aa970ac1072c866075ff2b043723cc
Second-Burdman <richardburdhealthcare@gmail.com>
2017-09-12 14:52:42 -0600

    added rest of methods to ProblematicWeather

--- lib/cli.rb
+++ lib/cli.rb
@@ -28,12 +28,44 @@ module ProblematicWeatherDefined
       end
     end
   end
+
+  def too_cold?(database, parameter)
+    database.all.each do |hour| 
+      if hour.temperature < parameter 
+        hour.problems << "It's gona be too cold this hour!"
+      end
+    end
+  end
+
+  def too_rainy?(database, parameter)
+    database.all.each do |hour| 
+      if hour.rain > parameter 
+        hour.problems << "It's probably gonna rain this hour!"
+      end
+    end
+  end 
+
+  def too_windy?(database, parameter)
+    database.all.each do |hour| 
+      if hour.temperature > parameter 
+        hour.problems << "It's gona be too windy this hour!"
+      end
+    end
+  end  
+  
 end 
 
 module CheckForProblematicWeather
   def run_parameters_against_problematic_criteria 
     too_hot?(WeatherDatabase, hot_parameter)
+    too_cold?(WeatherDatabase, cold_parameter)
+    too_rainy?(WeatherDatabase, rain_parameter)
+    too_windy?(WeatherDatabase, wind_parameter)
   end 
+  
+  def is_there_any_problamatic_weather? 
+    !(!(WeatherDatabase.all.detect {|hour| hour.problems.length != 0}))
+  end
 end 
 
 class WeatherParameters 
