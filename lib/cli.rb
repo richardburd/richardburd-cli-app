6da1ecb5cc0c4389c09a72506b9e7377289ce27b
Second-Burdman <richardburdhealthcare@gmail.com>
2017-09-12 14:55:36 -0600

    added list_out_hours_with_problematic_weather

--- lib/cli.rb
+++ lib/cli.rb
@@ -66,6 +66,20 @@ module CheckForProblematicWeather
   def is_there_any_problamatic_weather? 
     !(!(WeatherDatabase.all.detect {|hour| hour.problems.length != 0}))
   end
+
+  def list_out_hours_with_problamatic_weather 
+    WeatherDatabase.all.map do |hour|
+      if hour.problems.length > 0 
+        puts "Problamatic Conditions for: #{hour.time}"
+        counter = 0
+        while hour.problems.length > counter
+          puts "   #{counter + 1}.) #{hour.problems[counter]}"
+          counter += 1
+        end 
+        puts ""
+      end 
+    end
+  end 
 end 
 
 class WeatherParameters 
