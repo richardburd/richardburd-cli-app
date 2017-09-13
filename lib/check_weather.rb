4c6a3ed7771a23ec052c20df154e1958e2371a75
Richard Burd <richardburdx@yahoo.com>
2017-09-13 16:27:00 +0000

    added data_requester and check_weather folders

---
+++ lib/check_weather.rb
@@ -0,0 +1,7 @@
+require_relative '../config/environment'
+
+# you don't need this if you have the require_all 'lib' line on the environment
+# and you have the bundler gem properly installed.  This is here so I can learn how
+# to use require statements in lieu of the bundler gem, in case for some reason I'm
+# not using it.
+require_relative 'modules'
--- lib/weather_parameters.rb
+++ lib/weather_parameters.rb
@@ -1,7 +1,10 @@
 require_relative '../config/environment'
 
-#  you don't need this if you have the require_all 'lib' line on the environment
-#  require_relative 'modules'
+# you don't need this if you have the require_all 'lib' line on the environment
+# and you have the bundler gem properly installed.  This is here so I can learn how
+# to use require statements in lieu of the bundler gem, in case for some reason I'm
+# not using it.
+require_relative 'modules'
 
 class WeatherParameters
   attr_accessor :hot_parameter, :cold_parameter, :rain_parameter, :wind_parameter
